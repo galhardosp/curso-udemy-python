80eb1b2da9fde38fc40a8f844652e3f1844b6d7e
Jefferson Galhardo <jeffersongjunior@gmail.com>
2025-04-06 22:55:06 -0300

    aula 17

---
+++ aula15.py
@@ -0,0 +1,11 @@
+#função input
+
+# nome = input('qual seu nome? ')
+
+numero1 = input('Digite um numero: ') 
+numero2 = input('digite outro numero: ')
+
+int_numero1 = int(numero1)
+int_numero2 = int(numero2)
+
+print(f'A soma dos numeros é: {int_numero1 + int_numero2}')
---
+++ aula16.py
@@ -0,0 +1,8 @@
+pergunta = input('Digite entrar ou sair: ')
+
+if pergunta == 'entrar':
+    print('vc entrou')
+elif pergunta == 'sair':
+   print('vc saiu')
+else:
+    print('Vc não digitou nem entrar nem sair')
